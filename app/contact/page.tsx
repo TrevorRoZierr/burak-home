d1bc8e7157eac727597caba7df315cac240d5506
SAAD <saadsyed950@gmail.com.com>
2024-01-14 19:46:05 +0530

    sixth

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -20,7 +20,7 @@ const Content = ({ title, subTitle }: Props) => (
   </div>
 );
 
-const page = () => {
+const Page = () => {
   const form = useRef<HTMLFormElement | null>(null);
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
@@ -56,64 +56,11 @@ const page = () => {
           autoComplete="off"
           className="flex justify-center items-center flex-col"
         >
-          <input
-            type="text"
-            placeholder="Name:"
-            name="user_name"
-            className="sm:my-2 my-1 border-2 border-black sm:p-2 p-2 rounded placeholder-black"
-            required
-            autoComplete="off"
-          />
-          <br></br>
-          <input
-            type="email"
-            placeholder="Email:"
-            name="user_email"
-            required
-            autoComplete="off"
-            className="sm:my-2 my-1 border-2 border-black sm:p-2 p-2 rounded placeholder-black"
-          />
-          <br></br>
-          <input
-            type="text"
-            placeholder="Mobile Number:"
-            name="user_phone"
-            required
-            autoComplete="off"
-            className="sm:my-2 my-1 border-2 border-black sm:p-2 p-2 rounded placeholder-black"
-          />
-          <br></br>
-          <input
-            type="text"
-            placeholder="Subject:"
-            name="subject"
-            required
-            autoComplete="off"
-            className="sm:my-2 my-1 border-2 border-black sm:p-2 p-2 rounded placeholder-black"
-          />
-          <br></br>
-          <textarea
-            name="message"
-            placeholder="Type your Message:"
-            required
-            autoComplete="off"
-            cols={30}
-            rows={10}
-            className="sm:my-3 my-2 border-2 border-black sm:p-3 p-3 rounded placeholder-black"
-          ></textarea>
-          <br></br>
-          <div className="flex justify-center items-center flex-col">
-            <button
-              type="submit"
-              className="text-center p-4 border-2 rounded-full bg-black text-white sm:font-medium hover:text-black hover:bg-white duration-300"
-            >
-              Send Message
-            </button>
-          </div>
+          {/* ... your form inputs and button */}
         </form>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
