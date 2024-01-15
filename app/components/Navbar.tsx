3a204be16f436e1ae92932e6fdc10435d44b3161
SAAD <saadsyed950@gmail.com.com>
2024-01-15 21:54:43 +0530

    XV

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,9 @@
-import Link from "next/link";
 import styles from "./Navbar.module.css";
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   return (
     <div>
       <div className="flex w-full sm:p-5 p-3 bg-gradient-to-r from-[#71B6CF] to-cyan-900 border-b-4">
-        {/* <img
-          id="nav"
-          src="/logo.png"
-          alt="Logo"
-          className="sm:ml-5 ml-1 sm:w-[450px] sm:h-[450px] w-[150px] h-[150px] rounded-full"
-        /> */}
         <h1 className={styles.logo}>Burak Solutions</h1>
       </div>
       <div className="sm:w-full sm:h-full flex justify-center items-center">
